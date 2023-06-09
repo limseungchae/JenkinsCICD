1e79c20a416779150a97effd16ca864d163706f0
limseungchae <tmdco12@naver.com>
2023-06-09 12:04:13 +0900

    프로젝트 저장소 초기화

---
+++ src/main/java/seung/hello/jekins/jenkinscicd/JenkinsCicdApplication.java
@@ -0,0 +1,13 @@
+package seung.hello.jekins.jenkinscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCicdApplication.class, args);
+    }
+
+}
